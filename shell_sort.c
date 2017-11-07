c054b9debe3c65b676759f2f9d760fc8b8b74bd8
winderica <winderica@gmail.com>
2017-11-07 19:30:37 +0800

    fix bug

--- shell_sort.c
+++ shell_sort.c
@@ -1,28 +1,27 @@
-
 void shellSort(int a[],int len) {
-    int h = 1;
-    while (h < len) {
-        h *= 3;
-        h += 1;
+    int h = 0;
+    while (h < len / 3) {
+        h = 3 * h + 1;
     }
-    while (h > 1) {
+    while (h > 0) {
         for (int i = h; i < len; i++) {
-            for (int j = i; j > h; j--) {
-                if (a[j] < a[j - 1]){
+            for (int j = i; j >= h; j -= h) {
+                if (a[j] < a[j - h]) {
                     int t = a[j];
-                    a[j] = a[j - 1];
-                    a[j - 1] = t;
+                    a[j] = a[j - h];
+                    a[j - h] = t;
                 } else {
                     break;
                 }
             }
         }
+
         h /= 3;
     }
 }
 
 int main() {
-    int a[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    shellSort(a, 10);
+    int a[20] = {9, 4, 13, 3, 17, 5, 18, 11, 8, 12, 2, 1, 0, 19, 6, 14, 7, 15, 16, 10};
+    shellSort(a, 20);
     return 0;
 }
